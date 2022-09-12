1e10846b27206ae977c24f5682b91e687baffbc6
Owner <lama76618@gmail.com>
2022-09-12 14:11:04 +0300

    SplashScreen code finished, next time test it

--- app/src/main/java/ow/nardeen/nardeentaskmanager/SplashScreen.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SplashScreen.java
@@ -2,7 +2,9 @@ package ow.nardeen.nardeentaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
+
+        //start next activity/screen otamaticly after period of time
+        //فتح الشاشة التالية تلقائيا بعد فترة من الزمن
+        //ننعرف كائن من نوع Handler
+        //
+        Handler h=new Handler(); //تعريف كائن من نوع Handler
+        Runnable r=new Runnable() {
+            @Override
+            public void run() { //anonimos class
+                Intent i=new Intent(SplashScreen.this,SignIn.class);
+                        startActivity(i);
+
+              finish();//اغلاق الشاشة الحالية
+
+            }
+        };
+
+        h.postDelayed(r,3000);
+        }
 }
