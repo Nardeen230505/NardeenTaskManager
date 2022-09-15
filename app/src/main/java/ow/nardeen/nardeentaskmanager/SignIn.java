c9f8c346f7096db94af02f89ce5fce61facb8421
Owner <lama76618@gmail.com>
2022-09-15 11:23:34 +0300

    SignUp code is done and tested successfully

--- app/src/main/java/ow/nardeen/nardeentaskmanager/SignIn.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SignIn.java
@@ -2,13 +2,38 @@ package ow.nardeen.nardeentaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignIn extends AppCompatActivity {
+ private TextInputEditText etEmail;
+ private TextInputEditText etPassword;
+ private Button btnSignUp;
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnSignIn=findViewById(R.id.btnSignIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1=new Intent(SignIn.this,SignUp.class);
+                startActivity(i1);
+            }
+        });
+
     }
 }
--- app/src/main/java/ow/nardeen/nardeentaskmanager/SignUp.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SignUp.java
@@ -3,12 +3,25 @@ package ow.nardeen.nardeentaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
+private TextInputEditText etEmail2;
+private TextInputEditText etPassword;
+private TextInputEditText etRepassword;
+private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etEmail2=findViewById(R.id.etEmail2);
+        etPassword=findViewById(R.id.etPassword);
+        etRepassword=findViewById(R.id.etRepassword);
+        btnSave=findViewById(R.id.btnSave);
     }
 }
