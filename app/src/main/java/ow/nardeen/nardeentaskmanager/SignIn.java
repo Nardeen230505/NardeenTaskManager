6af3b12ee236a397846485826c31a308b423842d
Owner <lama76618@gmail.com>
2022-09-19 14:14:18 +0300

    event oriented programming

--- app/src/main/java/ow/nardeen/nardeentaskmanager/SignIn.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SignIn.java
@@ -20,16 +20,16 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_in); //يبني واحهة المستعمل بحيث تبني كل الكائنات الموجودة بملف التنسيق xml
+
 
         etEmail=findViewById(R.id.etEmail);
         etPassword=findViewById(R.id.etPassword);
         btnSignUp=findViewById(R.id.btnSignUp);
         btnSignIn=findViewById(R.id.btnSignIn);
-
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view ) { //الي بين قوسين هو بارامتر الي تسبب بالحدث
                 Intent i1=new Intent(SignIn.this,SignUp.class);
                 startActivity(i1);
             }
@@ -37,3 +37,6 @@ public class SignIn extends AppCompatActivity {
 
     }
 }
+/*  btnSignIn=findViewById(R.id.btnSignIn); // هادا null لانو عم بدور على الكائن وبعدني مش بانية واجهة المستعمل
+    setContentView(R.layout.activity_sign_in);
+ */
--- app/src/main/java/ow/nardeen/nardeentaskmanager/SplashScreen.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen); //يبني واحهة المستعمل بحيث تبني كل الكائنات الموجودة بملف التنسيق xml
 
         //start next activity/screen otamaticly after period of time
         //فتح الشاشة التالية تلقائيا بعد فترة من الزمن
