f877484fb94900757ceda95905f0857fa27f9c89
Owner <lama76618@gmail.com>
2022-09-21 15:02:22 +0300

    working on sign up

--- app/src/main/java/ow/nardeen/nardeentaskmanager/MainActivity.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/MainActivity.java
@@ -3,13 +3,21 @@ package ow.nardeen.nardeentaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SearchView search;
+    private ListView list;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///ujilllkjkjkl
+
+
     }
 }
--- app/src/main/java/ow/nardeen/nardeentaskmanager/SignUpActivity.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/SignUpActivity.java
@@ -3,9 +3,11 @@ package ow.nardeen.nardeentaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 private TextInputEditText etEmail2;
@@ -22,7 +24,14 @@ private Button btnSave;
         etEmail2=findViewById(R.id.etEmail2);
         etPassword=findViewById(R.id.etPassword);
         etRepassword=findViewById(R.id.etRepassword);
-        btnSave=findViewById(R.id.btnSave);
+        btnSave=findViewById(R.id.btnSignUpSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckAndSave();
+            }
+        });
     }
 
     private void CheckAndSave()
@@ -32,5 +41,27 @@ private Button btnSave;
         String rePassword=etRepassword.getText().toString();
 
         boolean isOk=true;
+
+        if(email.length()*password.length()*rePassword.length()==0)
+        {
+            etEmail2.setError("One of the files is empty");
+        }
+
+        if(password.equals(rePassword)==false)
+        {
+            etRepassword.setError("Is not equal to password");
+            isOk=false;
+        }
+
+        if(isOk)
+        {
+            FirebaseAuth auth1=FirebaseAuth.getInstance();
+            auth1.signInWithEmailAndPassword(email,password);
+        }
+
+
+
+
+
     }
 }
