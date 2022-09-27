6775f133aef8452c0a3b3c8afc6ed4d4a0504264
Owner <lama76618@gmail.com>
2022-09-27 13:24:38 +0300

    הפעלת מניו ومعالة حدث الاختيار في ال main activity

--- app/src/main/java/ow/nardeen/nardeentaskmanager/MainActivity.java
+++ app/src/main/java/ow/nardeen/nardeentaskmanager/MainActivity.java
@@ -1,9 +1,12 @@
 package ow.nardeen.nardeentaskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -28,7 +31,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(in);
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        //دالة مسؤولة عن تشغيل الmenu
+        //دالة بوليانية ترجع ترو اذا كل اشي تمام
+        //بعد ما بنيت المنيو لازم المنيو لما اعمل هرتسا
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    // هذه الدالة عبارة عن ردة فعل لما بختار الحدث
+    {
+        if (item.getItemId()==R.id.itmSettings)
+        {
+            Intent intentS=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intentS);
+        }
+
+        if (item.getItemId()==R.id.itmHistory)
+        {
+            Intent iH=new Intent(MainActivity.this,HistoryActivity.class);
+            startActivity(iH);
+        }
+
+        if (item.getItemId()==R.id.itmSignOut)
+        {
+            Intent iSO=new Intent(MainActivity.this,SignInActivity.class);
+            startActivity(iSO);
+        }
+        return true;
     }
 }
